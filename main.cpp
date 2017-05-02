115c60dcb2302856fa7eb70f5c17b66397450a92
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2017-05-02 22:03:31 +0200

    Decode option (-d/--decode) with Unicode support, two code paths

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <map>
 #include <string>
 #include <iostream>
+#include <locale>
 
 struct Arg: public option::Arg
 {
@@ -43,15 +44,16 @@ struct Arg: public option::Arg
 };
  
 
-enum  optionIndex { UNKNOWN, HELP, REV, PATH };
+enum  optionIndex { UNKNOWN, HELP, REV, PATH, DECODE };
 const option::Descriptor usage[] =
 {
  { UNKNOWN, 0, "", "", option::Arg::None, "USAGE: cgiturl <URL> [options]\n"
                                           "       cgiturl -d <GitURL|gcode>\n\n"
                                           "Options:" },
- { HELP,    0, "h", "help",     option::Arg::None, " -h, --help      Print help and exit" },
- { REV,     0, "r", "revision", Arg::NonEmpty,     " -r, --revision  Which revision to encode in GitURL" },
- { PATH,    0, "p", "path",     Arg::NonEmpty,     " -p, --path      A path to encode in GitURL" },
+ { HELP,    0, "h", "help",     option::Arg::None, " -h, --help              Print help and exit" },
+ { REV,     0, "r", "revision", Arg::NonEmpty,     " -r <r>, --revision <r>  Which revision to encode in GitURL" },
+ { PATH,    0, "p", "path",     Arg::NonEmpty,     " -p <p>, --path <p>      A path to encode in GitURL" },
+ { DECODE,  0, "d", "decode",   Arg::NonEmpty,     " -d <c>, --decode <c>    GitURL or gcode to decode" },
 
  { UNKNOWN, 0, "", "",          option::Arg::None, "\nExamples:\n"
                                "  giturl https://github.com/zdharma/giturl -p lib/codes_huffman\n"
@@ -72,46 +74,51 @@ int main( int argc, char * argv[]) {
     std::vector<option::Option> buffer( stats.buffer_max );
     option::Parser parse( true, usage, argc, argv, &options[0], &buffer[0] );
 
-    if (parse.error()) {
+    if ( parse.error() ) {
         fprintf( stderr, "Error when parsing options, aborting\n" );
         fflush( stderr );
         return 1;
     }
 
-    if (options[HELP] || argc == 0) {
+    if ( options[HELP] ) {
         option::printUsage(std::cout, usage);
         return 0;
     }
 
-    std::string site="2";
-    std::string repo="psprint/zkl";
-    std::string rev;
-    if ( options[REV].count() > 0 ) {
-        rev = std::string( options[REV].last()->arg );
-    }
-    std::string file;
-    if ( options[PATH].count() > 0 ) {
-        file = std::string( options[PATH].last()->arg );
-    }
-    std::vector<int> selectors;
+    if ( options[DECODE].count() == 0 ) {
+        std::string site="2";
+        std::string repo="psprint/zkl";
+        std::string rev;
+        if ( options[REV].count() > 0 ) {
+            rev = std::string( options[REV].last()->arg );
+        }
+        std::string file;
+        if ( options[PATH].count() > 0 ) {
+            file = std::string( options[PATH].last()->arg );
+        }
+        std::vector<int> selectors;
 
-    std::wstring gcode = build_gcode( site, repo, rev, file, selectors );
-    std::wcout << gcode << std::endl;
-
-    std::vector<int> bits;
-    int error;
-    tie( bits, error ) = decode_zcode( gcode );
-    if( error != 0 ) {
-        std::cout << "Error during decoding the entered Zcode: " << error << std::endl;
+        std::wstring gcode = build_gcode( site, repo, rev, file, selectors );
+        std::wcout << gcode << std::endl;
     } else {
-        int to_skip;
-        std::map< std::string, std::string > decoded;
-        tie( to_skip, decoded, error ) = process_meta_data( bits );
+        std::wstring gcode( strlen( options[DECODE].last()->arg ), L' ');
+        gcode.resize( std::mbstowcs( &gcode[0], options[DECODE].last()->arg, strlen( options[DECODE].last()->arg ) ) );
 
-        std::cout << "Site: " << decoded["site"] << std::endl;
-        std::cout << "Repo: " << decoded["repo"] << std::endl;
-        std::cout << "Rev: " << decoded["rev"] << std::endl;
-        std::cout << "File: " << decoded["file"] << std::endl;
+        std::vector<int> bits;
+        int error;
+        tie( bits, error ) = decode_zcode( gcode );
+        if( error != 0 ) {
+            std::cout << "Error during decoding the entered Zcode: " << error << std::endl;
+        } else {
+            int to_skip;
+            std::map< std::string, std::string > decoded;
+            tie( to_skip, decoded, error ) = process_meta_data( bits );
+
+            std::cout << "Site: " << decoded["site"] << std::endl;
+            std::cout << "Repo: " << decoded["repo"] << std::endl;
+            std::cout << "Rev: " << decoded["rev"] << std::endl;
+            std::cout << "File: " << decoded["file"] << std::endl;
+        }
     }
     return 0;
 }
